d7bcca4061ed72b7b8e1c766c3ca30ae1a801251
ZiaSaid <121418141+ZiaSaid@users.noreply.github.com>
2023-01-14 21:48:57 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 print("This is not worthy")
-print("Hello Ahmad to create conflict  try it next time")
+print("Hello Ahmad to create conflict  try it next time hoihoinnjnio")
 print("Merge conflict")
